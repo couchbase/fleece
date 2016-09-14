91aeac3942ee201cfd8fa2b48725dc9c1e989aa2
Jim Borden <jim.borden@couchbase.com>
2016-09-15 06:51:36 +0900

    More MSVC compatibility defines

--- Fleece/MSVC_Compat.hh
+++ Fleece/MSVC_Compat.hh
@@ -25,6 +25,15 @@
     #include <BaseTsd.h>
     typedef SSIZE_T ssize_t;
 
+    #define MKDIR(PATH, MODE) ::_mkdir(PATH)
+    #define chmod ::_chmod
+    #define fdopen ::_fdopen
+
+    #define srandomdev() 
+    #define MAXFLOAT FLT_MAX
+
+    #define __printflike(A, B) 
+
     // MSVC doesn't support C99 so it doesn't have variable-length C arrays.
     // WARNING: sizeof() will not work on this array since it's actually declared as a pointer.
     #define StackArray(NAME, TYPE, SIZE)    TYPE* NAME = (TYPE*)_malloca(sizeof(TYPE)*(SIZE))
@@ -33,6 +42,8 @@
 
     #define _usuallyTrue(VAL)               __builtin_expect(VAL, true)
     #define _usuallyFalse(VAL)              __builtin_expect(VAL, false)
+
+    #define MKDIR(PATH, MODE) ::mkdir(PATH, (mode_t)MODE)
 
     #define StackArray(NAME, TYPE, SIZE)    TYPE NAME[(SIZE)]
 
