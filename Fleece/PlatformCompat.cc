7aceb28a77485a605642ea27c8974d24aed2d1d3
Jim Borden <jim.borden@couchbase.com>
2016-11-25 08:53:02 +0900

    Found a more succint way to migrate functions to their wide alternatives

--- Fleece/PlatformCompat.cc
+++ Fleece/PlatformCompat.cc
@@ -1,53 +1,48 @@
+#ifdef WIN32
+
 #include "PlatformCompat.hh"
 #include <direct.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <wchar.h>
-#include <Windows.h>
+#include <atlbase.h>
+#include <atlconv.h>
 
-#define u8_to_wstr(in, out_name, bad_val) wchar_t out_name[256]; \
- if(MultiByteToWideChar(CP_UTF8, 0, in, strnlen_s(in, 255) + 1, out_name, 256) == 0) errno = EINVAL; return bad_val
+#define MIGRATE_1(from, to) from(const char* const arg1) { \
+CA2WEX<256> wide(arg1, CP_UTF8); \
+return to(wide); \
+}
+
+#define MIGRATE_2(from, to, type1) from(const char* const arg1, type1 arg2) { \
+CA2WEX<256> wide(arg1, CP_UTF8); \
+return to(wide, arg2); \
+}
+
+#define MIGRATE_2S(from, to) from(const char* const arg1, const char* const arg2) { \
+CA2WEX<256> wide1(arg1, CP_UTF8); \
+CA2WEX<256> wide2(arg2, CP_UTF8); \
+return to(wide1, wide2); \
+}
+
+#define MIGRATE_ARG(arg, retVal) CA2WEX<256> w##arg(arg, CP_UTF8); return retVal
+
 
 int mkdir_u8(const char *path, int mode) {
-    u8_to_wstr(path, wname, -1);
-
-    return _wmkdir(wname);
+    MIGRATE_ARG(path, ::_wmkdir(wpath));
 }
 
 int stat_u8(const char* const filename, struct stat * const s)
 {
-    u8_to_wstr(filename, wname, -1);
-    return ::_wstat64i32(wname, (struct _stat64i32 *)s);
+    MIGRATE_ARG(filename, ::_wstat64i32(wfilename, (struct _stat64i32 *)s));
 }
 
-int rmdir_u8(const char* const path)
-{
-    u8_to_wstr(path, wname, -1);
-    return ::_wrmdir(wname);
-}
+int MIGRATE_1(rmdir_u8, ::_wrmdir)
 
-int rename_u8(const char* const oldPath, const char* const newPath)
-{
-    u8_to_wstr(oldPath, woldPath, -1);
-    u8_to_wstr(newPath, wnewPath, -1);
-    return ::_wrename(woldPath, wnewPath);
-}
+int MIGRATE_2S(rename_u8, ::_wrename)
 
-int unlink_u8(const char* const filename)
-{
-    u8_to_wstr(filename, wname, -1);
-    return ::_wunlink(wname);
-}
+int MIGRATE_1(unlink_u8, ::_wunlink)
 
-int chmod_u8(const char* const filename, int mode)
-{
-    u8_to_wstr(filename, wname, -1);
-    return ::_wchmod(wname, mode);
-}
+int MIGRATE_2(chmod_u8, ::_wchmod, int)
 
-FILE* fopen_u8(const char* const path, const char* const mode)
-{
-    u8_to_wstr(path, wname, nullptr);
-    u8_to_wstr(mode, wmode, nullptr);
-    return ::_wfopen(wname, wmode);
-}
+FILE* MIGRATE_2S(fopen_u8, ::_wfopen)
+
+#endif
