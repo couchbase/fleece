3884b9253dec10d2783e5ac22231615951c2e153
Jim Borden <jim.borden@couchbase.com>
2016-11-29 15:00:45 +0900

    Need to explicitly namespace implementation, oops!

--- Fleece/PlatformCompat.cc
+++ Fleece/PlatformCompat.cc
@@ -7,19 +7,17 @@
 #include <atlbase.h>
 #include <atlconv.h>
 
-using namespace fleece;
-
-#define MIGRATE_1(from, to) from(const char* const arg1) { \
+#define MIGRATE_1(from, to) fleece::from(const char* const arg1) { \
 CA2WEX<256> wide(arg1, CP_UTF8); \
 return to(wide); \
 }
 
-#define MIGRATE_2(from, to, type1) from(const char* const arg1, type1 arg2) { \
+#define MIGRATE_2(from, to, type1) fleece::from(const char* const arg1, type1 arg2) { \
 CA2WEX<256> wide(arg1, CP_UTF8); \
 return to(wide, arg2); \
 }
 
-#define MIGRATE_2S(from, to) from(const char* const arg1, const char* const arg2) { \
+#define MIGRATE_2S(from, to) fleece::from(const char* const arg1, const char* const arg2) { \
 CA2WEX<256> wide1(arg1, CP_UTF8); \
 CA2WEX<256> wide2(arg2, CP_UTF8); \
 return to(wide1, wide2); \
@@ -28,11 +26,11 @@ return to(wide1, wide2); \
 #define MIGRATE_ARG(arg, retVal) CA2WEX<256> w##arg(arg, CP_UTF8); return retVal
 
 
-int mkdir_u8(const char *path, int mode) {
+int fleece::mkdir_u8(const char *path, int mode) {
     MIGRATE_ARG(path, ::_wmkdir(wpath));
 }
 
-int stat_u8(const char* const filename, struct ::stat * const s)
+int fleece::stat_u8(const char* const filename, struct stat * const s)
 {
     MIGRATE_ARG(filename, ::_wstat64i32(wfilename, (struct _stat64i32 *)s));
 }
