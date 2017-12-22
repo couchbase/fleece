6e2a0f3efb857cd3411d7fc8c8e168239d0f86af
Jim Borden <jim.borden@couchbase.com>
2017-12-22 13:57:07 +0900

    [C#] Update literal binding for FLDictKey

--- Fleece/templates/FLDictKey_literal.cs
+++ Fleece/templates/FLDictKey_literal.cs
@@ -7,12 +7,15 @@
     {
         #pragma warning disable CS0169
 
-        // _private1[3] 
+        // _private1[4] 
         private void* _private1a;
         private void* _private1b;
         private void* _private1c;
+        private void* _private1d;
         private uint _private2;
-        private byte _private3;
+        private uint _private3;
+        private byte _private4;
+        private byte _private5;
 
         #pragma warning restore CS0169
     }
