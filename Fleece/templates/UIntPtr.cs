db5ff9034c73be0aae8413897711966177bacab2
Jim Borden <jim.borden@couchbase.com>
2018-03-27 10:15:08 +0900

    Move bindings scripts out of repo

--- Fleece/templates/UIntPtr.cs
+++
@@ -1,9 +0,0 @@
-        public ulong {0}
-        {{
-            get {{
-                return _{0}.ToUInt64();
-            }}
-            set {{
-                _{0} = (UIntPtr)value;
-            }}
-        }}
--- Fleece/templates/bool.cs
+++
@@ -1,9 +0,0 @@
-        public bool {0}
-        {{
-            get {{
-                return Convert.ToBoolean(_{0});
-            }}
-            set {{
-                _{0} = Convert.ToByte(value);
-            }}
-        }}
