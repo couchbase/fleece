e30c97c34bfd97963b0d35110952f153994c331b
Jim Borden <jim.borden@couchbase.com>
2018-08-15 08:20:49 +0900

    Add accidentally ignored build script

---
+++ build_cmake/build.ps1
@@ -0,0 +1,30 @@
+param(
+    [Parameter(Mandatory=$true)][string]$Platform
+)
+
+if($Platform -eq "x86" -or $Platform -eq "Win32") {
+    pushd build_cmake/x86
+    cmake -G "Visual Studio 14 2015" ..\..
+    cmake --build . --config RelWithDebInfo --target fleeceTool
+    popd
+
+    pushd build_cmake/x86_store
+    cmake -G "Visual Studio 14 2015" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION="10.0.14393.0" -DCMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION="10.0.10240.0" ..\..
+    cmake --build . --config RelWithDebInfo --target Fleece
+    popd
+} elseif($Platform -eq "x64" -or $Platform -eq "Win64") {
+    pushd build_cmake/x64
+    cmake -G "Visual Studio 14 2015 Win64" ..\..
+    cmake --build . --config RelWithDebInfo --target fleeceTool
+    popd
+
+    pushd build_cmake/x64_store
+    cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION="10.0.14393.0" -DCMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION="10.0.10240.0" ..\..
+    cmake --build . --config RelWithDebInfo --target Fleece
+    popd
+} else {
+    pushd build_cmake/arm
+    cmake -G "Visual Studio 14 2015 ARM" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION="10.0.14393.0" -DCMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION="10.0.10240.0" ..\..
+    cmake --build . --config RelWithDebInfo --target Fleece
+    popd
+}
