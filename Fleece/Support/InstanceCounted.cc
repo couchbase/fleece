1cc34cfcff9e8c10fca1148d171efab088802830
Jens Alfke <jens@couchbase.com>
2018-11-16 17:30:31 -0800

    Added InstanceCounted utility
    Moved here from LiteCore, and improved

---
+++ Fleece/Support/InstanceCounted.cc
@@ -0,0 +1,82 @@
+//
+// InstanceCounted.cc
+//
+// Copyright © 2018 Couchbase. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#include "InstanceCounted.hh"
+#include "RefCounted.hh"
+#include <map>
+#include <mutex>
+
+#if defined(__clang__) // For __cxa_demangle
+#include <cxxabi.h>
+#endif
+
+namespace fleece {
+    using namespace std;
+
+    std::atomic<int> InstanceCounted::gInstanceCount;
+
+// LCOV_EXCL_START
+#if INSTANCECOUNTED_TRACK
+    static mutex sInstancesMutex;
+    static map<const InstanceCounted*,size_t> sInstances;
+
+    void InstanceCounted::track(size_t offset) const {
+        // `offset` is the offset from `this` (the InstanceCounted instance) to the main object
+        // it's part of. This is stored in the map so it can be used to log the address of the main
+        // object, which is the one we really care about.
+        lock_guard<mutex> lock(sInstancesMutex);
+        sInstances.insert({this, offset});
+        ++gInstanceCount;
+    }
+
+    void InstanceCounted::untrack() const {
+        lock_guard<mutex> lock(sInstancesMutex);
+        sInstances.erase(this);
+        --gInstanceCount;
+    }
+
+    void InstanceCounted::dumpInstances() {
+        char* unmangled = nullptr;
+        lock_guard<mutex> lock(sInstancesMutex);
+        for (auto entry : sInstances) {
+            const char *name =  typeid(*entry.first).name();
+            const void *address = (uint8_t*)entry.first - entry.second;
+    #ifdef __clang__
+            int status;
+            size_t unmangledLen = 0;
+            unmangled = abi::__cxa_demangle(name, unmangled, &unmangledLen, &status);
+            if (unmangled && status == 0)
+                name = unmangled;
+    #endif
+
+            fprintf(stderr, "    * %s at %p  [", name, address);
+            for (int i=0; i < 4; i++) {
+                if (i > 0)
+                    fprintf(stderr, " ");
+                fprintf(stderr, "%08x", ((uint32_t*)address)[i]);
+            }
+            fprintf(stderr, "]\n");
+        }
+        free(unmangled);
+    }
+
+#endif // INSTANCECOUNTED_TRACK
+// LCOV_EXCL_STOP
+
+
+}
---
+++ Fleece/Support/InstanceCounted.hh
@@ -0,0 +1,78 @@
+//
+// InstanceCounted.hh
+//
+// Copyright © 2018 Couchbase. All rights reserved.
+//
+
+#pragma once
+#include <atomic>
+#include <stdint.h>
+
+#ifndef INSTANCECOUNTED_TRACK
+    #if DEBUG
+        #define INSTANCECOUNTED_TRACK 1
+    #endif
+#endif
+
+namespace fleece {
+
+    /** Base class that keeps track of the total instance count of it and all subclasses.
+        This is useful for leak detection.
+        In debug builds or if INSTANCECOUNTED_TRACK is defined, the class will also track the
+        individual instance addresses, which can be logged by calling `dumpInstances`. */
+    class InstanceCounted {
+    public:
+
+        /** Total number of live objects that implement InstanceCounted. */
+        static int count()                          {return gInstanceCount;}
+
+#if INSTANCECOUNTED_TRACK
+        InstanceCounted()                           {track();}
+        InstanceCounted(const InstanceCounted&)     {track();}
+        virtual ~InstanceCounted()                  {untrack();}        // must be virtual for RTTI
+
+        /** Logs information to stderr about all live objects. */
+        static void dumpInstances();
+
+    protected:
+        InstanceCounted(size_t offset)              {track(offset);}
+    private:
+        void track(size_t offset =0) const;
+        void untrack() const;
+
+#else
+        InstanceCounted()                           {++gInstanceCount;}
+        InstanceCounted(const InstanceCounted&)     {++gInstanceCount;}
+        ~InstanceCounted()                          {--gInstanceCount;}
+#endif
+
+    private:
+        static std::atomic<int> gInstanceCount;
+    };
+
+
+    /** Alternative to InstanceCounted that must be used in cases where
+        - you're using multiple inheritance,
+        - InstanceCounted is not the first parent class listed,
+        - _and_ an earlier parent class has virtual methods.
+        In that situation, InstanceCounted won't be able to determine the exact address of the
+        object (due to the weird way C++ MI works), so instead you should use
+        InstanceCountedIn<MyClass>, where MyClass is the class you're declaring. For example:
+            class MyClass : public BaseClassWithVirtual, InstanceCounted<MyClass> { ... };
+        */
+    template <class BASE>
+    class InstanceCountedIn : public InstanceCounted {
+    public:
+#if INSTANCECOUNTED_TRACK
+        InstanceCountedIn()
+        :InstanceCounted((size_t)this - (size_t)(BASE*)this)
+        { }
+
+        InstanceCountedIn(const InstanceCountedIn&)
+        :InstanceCounted((size_t)this - (size_t)(BASE*)this)
+        { }
+#endif
+    };
+
+
+}
