9e00105306c1e8fd03c67afca2b21f84d54fffbb
Jens Alfke <jens@couchbase.com>
2019-08-27 14:53:40 -0700

    Base.hh: Added RETURNS_NONNULL, MUST_USE_RESULT

--- API/fleece/Base.hh
+++ API/fleece/Base.hh
@@ -6,17 +6,27 @@
 
 #pragma once
 
+// https://clang.llvm.org/docs/AttributeReference.html
+// https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html
 
-#ifdef __clang__
-    #define NONNULL                         __attribute__((nonnull))
-#else
-    #define NONNULL
-#endif
+#if defined(__clang__) || defined(__GNUC__)
+    #define RETURNS_NONNULL                 __attribute__((returns_nonnull))
+    #define MUST_USE_RESULT                 __attribute__((warn_unused_result))
 
-#ifdef _MSC_VER
-    #define _usuallyTrue(VAL)               (VAL)
-    #define _usuallyFalse(VAL)              (VAL)
-#else
     #define _usuallyTrue(VAL)               __builtin_expect(VAL, true)
     #define _usuallyFalse(VAL)              __builtin_expect(VAL, false)
+#else
+    #define RETURNS_NONNULL
+    #define MUST_USE_RESULT
+
+    #define _usuallyTrue(VAL)               (VAL)
+    #define _usuallyFalse(VAL)              (VAL)
+#endif
+
+
+#if defined(__clang__)
+    #define NONNULL                         __attribute__((nonnull))
+#else
+    // GCC does have __attribute__((nonnull)) but it works differently (requires argument numbers)
+    #define NONNULL
 #endif
