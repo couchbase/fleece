ad40d5b20c4139f85fa04ccf22942141d4a4037c
Jens Alfke <jens@couchbase.com>
2021-04-27 15:08:03 -0700

    Preserve addresses of shared key strings on txn abort (CBL-1707)
    PersistentSharedKeys::revert() was creating a new ConcurrentMap to
    store the remaining keys, since ConcurrentMap didn't support deletion.
    Unfortunately that meant that the existing keys had different
    addresses afterwards, invalidating anything that was holding onto them
    as slices. That included the Obj-C mutable-Fleece classes.
    
    Fixed this by implementing ConcurrentMap::remove(), which can remove
    keys in place, removing the need to create a new map.
    
    CBL-1707

--- Fleece/Support/ConcurrentMap.hh
+++ Fleece/Support/ConcurrentMap.hh
@@ -13,20 +13,27 @@
 namespace fleece {
 
     /** A lockless concurrent hash table that maps strings to 16-bit ints.
-        Does not support modifications or deletions. Intended for use by SharedKeys. */
+        Intended for use by SharedKeys. */
     class ConcurrentMap {
         public:
         static constexpr int kMaxCapacity = 0x7FFF;
         static constexpr int kMaxStringCapacity = 0x10000;
 
+        /** Constructs a ConcurrentMap. The capacity is fixed.
+            @param capacity  The number of keys it needs to hold. Cannot exceed kMaxCapacity.
+            @param stringCapacity  Maximum total size in bytes of all keys, including one byte per
+                                   key as a separator. Cannot exceed kMaxStringCapacity.
+                                   If 0 or omitted, value is `17 * capacity`. */
         ConcurrentMap(int capacity, int stringCapacity =0);
 
         // Move cannot be concurrent with find or insert calls!
         ConcurrentMap(ConcurrentMap&&);
         ConcurrentMap& operator=(ConcurrentMap&&);
 
+        /// The type of value associated with a key.
         using value_t = uint16_t;
 
+        /// The hash code of a key.
         enum class hash_t : uint32_t { };
 
         /** Computes the hash code of a key. This code can be passed to alternate versions of the
@@ -44,13 +51,14 @@ namespace fleece {
             uint16_t value;
         };
 
-        /** Looks up the key. Returns the value, as well as the key in memory owned by the map.
+        /** Looks up the key. Returns the value, as well as the key in memory owned by the map
+            (which is guaranteed to remain valid until the entry is removed or the map destructed.)
             If the key is not found, returns a result with an empty slice for the key. */
         result find(slice key) const noexcept FLPURE    {return find(key, hashCode(key));}
         result find(slice key, hash_t) const noexcept FLPURE;
 
-        /** Inserts a value for a key. Returns the value, as well as a new copy of the key in memory owned
-            by the map.
+        /** Inserts a value for a key. Returns the value, as well as a new copy of the key in
+            memory owned by the map.
             If the key already exists, the existing value is not changed, and the existing value is
             returned as well as the managed copy of the key (as from `find`.)
             If the hash table or key storage is full and the key can't be inserted, returns an empty
@@ -58,12 +66,21 @@ namespace fleece {
         result insert(slice key, value_t value)         {return insert(key, value, hashCode(key));}
         result insert(slice key, value_t value, hash_t);
 
+        /** Removes the value for a key. Returns true if removed, false if not found.
+            \note  The space occupied by the key string can only be recovered if this was the
+                   last key added, so when removing multiple keys it's best to go in reverse
+                   chronological order. */
+        bool remove(slice key)                          {return remove(key, hashCode(key));}
+        bool remove(slice key, hash_t hash);
+
+        /** Writes all the table entries, plus statistics, to stdout. */
         void dump() const;
 
     private:
+        // Hash table entry (32 bits).
         struct Entry {
-            uint16_t keyOffset;               // 1 + offset of key from _keysOffset, or 0 if empty
-            uint16_t value;                   // value of key
+            uint16_t keyOffset;     // offset of key from _keysOffset, or 0 if empty, 1 if deleted
+            uint16_t value;         // value associated with key
 
             uint32_t& asInt32()                 {return *(uint32_t*)this;}
             bool compareAndSwap(Entry expected, Entry swapWith);
