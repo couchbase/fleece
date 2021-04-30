8fbc0c6ccbc6aba86db34b9ffd98391c376f5db3
Jens Alfke <jens@couchbase.com>
2021-04-30 15:07:22 -0700

    API: Added [Core]Foundation-to-Fleece conversions
    Unlike the existing `FLEncoder_WriteCFObject`, these functions create
    mutable object trees. They're needed by the formatted builder so it
    can accept complex CF/NS values using the `%@` specifier.

--- API/fleece/Fleece+CoreFoundation.h
+++ API/fleece/Fleece+CoreFoundation.h
@@ -36,6 +36,16 @@ extern "C" {
         Caller must CFRelease the result. */
     NODISCARD FLEECE_PUBLIC CFTypeRef FLValue_CopyCFObject(FLValue FL_NULLABLE) FLAPI;
 
+    /** Copies a CoreFoundation object to a standalone Fleece object, if possible.
+        @throws an NSException if the object is not of a class that can be converted, or if it
+                contains such an object.
+        \note  You must call \ref FLValue_Release when finished with the result. */
+    FLValue FLValue_FromCFValue(CFTypeRef);
+
+    /** Stores a CoreFoundation object into a slot in a Fleece dict/array, if possible.
+        This is supported for the CF/NS equivalents of Fleece types: CFString, CFNumber,
+        CFData, CFArray, CFDictionary. */
+    void FLSlot_SetCFValue(FLSlot slot, CFTypeRef value);
 
     /** Same as FLDictGet, but takes the key as a CFStringRef. */
     NODISCARD FLEECE_PUBLIC FLValue FLDict_GetWithCFString(FLDict FL_NULLABLE, CFStringRef) FLAPI;
@@ -68,6 +78,11 @@ extern "C" {
     /** Same as FLEncoder_Finish, but returns result as NSData or error as NSError. */
     FLEECE_PUBLIC NSData* FLEncoder_FinishWithNSData(FLEncoder, NSError** FL_NULLABLE) FLAPI;
 
+    /** Copies a Foundation object to a standalone Fleece object, if possible.
+        @throws an NSException if the object is not of a class that can be converted, or if it
+                contains such an object.
+        \note  You must call \ref FLValue_Release when finished with the result. */
+    FLValue FLValue_FromNSObject(id);
 
     /** NSError domain string for Fleece errors */
     FLEECE_PUBLIC extern NSString* const FLErrorDomain;
@@ -79,6 +94,22 @@ extern "C" {
         them. You can implement this method in your classes. In it, call the encoder to write
         a single object (which may of course be an array or dictionary.) */
     - (void) fl_encodeToFLEncoder: (FLEncoder)enc;
+
+    /** This method is called by \ref FLValue_FromNSObject and \ref FLValue_FromCFObject.
+        It's already implemented by NSString, NSNumber, NSData, NSArray, NSDictionary.
+        You can implement it in your own classes.
+        @return  A non-NULL retained Fleece value, i.e. one that the caller of this method will
+                have to call a "release" function on. Typically you'll create a mutable Fleece dict
+                or array, populate it, and return it without releasing it. */
+    - (FLValue) fl_convertToFleece;
+
+    /** This method is called by \ref FLSlot_SetCFValue.
+        It's already implemented by NSString, NSNumber, NSData, NSArray, NSDictionary.
+        Otherwise, the default implementation calls `-fl_convertToFleece` and stores the
+        resulting `FLValue` in the slot.
+        You could override this, but it's not usually useful. */
+    - (void) fl_storeInSlot: (FLSlot)slot;
+
     @end
 #endif
 
