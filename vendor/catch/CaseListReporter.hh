aca8f97339353f557bf4274a4c520b61c9da29c7
udkyo <blair.watt@couchbase.com>
2021-09-28 23:02:10 +0100

    CBD-4043: Mobile 3.0 BSL (#117)

--- vendor/catch/CaseListReporter.hh
+++ vendor/catch/CaseListReporter.hh
@@ -3,7 +3,13 @@
 //  Couchbase Lite Core
 //
 //  Created by Jens Alfke on 8/26/16.
-//  Copyright (c) 2016 Couchbase. All rights reserved.
+//  Copyright 2016-Present Couchbase, Inc.
+//
+//  Use of this software is governed by the Business Source License included
+//  in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
+//  in that file, in accordance with the Business Source License, use of this
+//  software will be governed by the Apache License, Version 2.0, included in
+//  the file licenses/APL2.txt.
 //
 
 #include "catch.hpp"
--- vendor/wyhash/wyhash32.h
+++ vendor/wyhash/wyhash32.h
@@ -1,3 +1,14 @@
+/*
+Copyright 2021-Present Couchbase, Inc.
+// Author: Wang Yi <godspeed_china@yeah.net>
+// Author: Wang Yi <godspeed_china@yeah.net>
+Use of this software is governed by the Business Source License included in
+the file licenses/BSL-Couchbase.txt.  As of the Change Date specified in that
+file, in accordance with the Business Source License, use of this software will
+be governed by the Apache License, Version 2.0, included in the file
+licenses/APL2.txt.
+*/
+
 // Author: Wang Yi <godspeed_china@yeah.net>
 #include <stdint.h>
 #include <string.h>
