f00f5575a8752318f8898bf4e41c437e08929e0d
Jianmin Zhao <jianmin.zhao@couchbase.com>
2021-11-24 17:53:56 -0800

    try not to fix the optional.

--- Fleece/Core/Value+Dump.cc
+++ Fleece/Core/Value+Dump.cc
@@ -167,7 +167,8 @@ namespace fleece { namespace impl {
                     } else {
                         auto dstPtr = ptr->deref(wide);
                         writeDumpBrief(dstPtr, true);
-                        offset = valueToOffset(dstPtr).value_or(0ll);
+//                        offset = valueToOffset(dstPtr).value_or(0ll);
+                        offset = *valueToOffset(dstPtr);
                     }
                     char buf[32];
                     if (offset >= 0)
