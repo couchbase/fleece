8e10e3d0243a1204d2bda589ea13f93404b0c6d2
callumbirks <73551885+callumbirks@users.noreply.github.com>
2023-06-20 14:20:36 +0100

    CBL-4471: Xcode 14.3 unqualified calls to std::move

--- Fleece/Support/Backtrace.cc
+++ Fleece/Support/Backtrace.cc
@@ -396,7 +396,7 @@ namespace fleece {
     void Backtrace::installTerminateHandler(function<void(const string&)> logger) {
         static once_flag sOnce;
         call_once(sOnce, [&] {
-            static auto const sLogger = move(logger);
+            static auto const sLogger = std::move(logger);
             static terminate_handler const sOldHandler = set_terminate([] {
                 // ---- Code below gets called by C++ runtime on an uncaught exception ---
                 if (sLogger) {
