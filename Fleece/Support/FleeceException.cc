d9e3f33863c8af46f540359f5986335fc714dced
Jianmin Zhao <jianmin.zhao@couchbase.com>
2023-11-10 09:36:31 -0800

    Add some comments per review feedbacks.

--- Fleece/Support/FleeceException.cc
+++ Fleece/Support/FleeceException.cc
@@ -49,6 +49,12 @@ namespace fleece {
     ,code(code_)
     ,err_no(errno_)
     {
+        // The Fleece iterator (of array and dict) throws OutOfRange exception as one moves
+        // the iterator, operator++ or FLArrayIterator_Next, for instances, beyond the
+        // end of the array. Although the exception is caught inside Fleece and the
+        // client gets return of false, the effort to capture the backtrace can be
+        // very significant as shown in performance profilings. And, the backtrace is not
+        // used anyway.
         if (code_ != OutOfRange) {
             backtrace = Backtrace::capture(2);
         }
