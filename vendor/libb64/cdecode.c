c6978bd7363534b8e23979c06d6410b5cd36454c
Jens Alfke <jens@couchbase.com>
2024-08-05 11:31:29 -0700

    Xcode: Enabled -Wimplicit-fallthrough

--- vendor/libb64/cdecode.c
+++ vendor/libb64/cdecode.c
@@ -7,6 +7,8 @@ For details, see http://sourceforge.net/projects/libb64
 
 #include "cdecode.h"
 
+#pragma clang diagnostic ignored "-Wimplicit-fallthrough"
+
 char base64_decode_value(uint8_t value_in)
 {
 	static const int8_t decoding[] = {62,-1,-1,-1,63,52,53,54,55,56,57,58,59,60,61,-1,-1,-1,-2,-1,-1,-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,-1,-1,-1,-1,-1,-1,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51};
